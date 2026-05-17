157987023ced5b536ff3f1daeafb1b0e618aca6f
engfeda-ui <eng.feda@gmail.com>
2026-05-17 14:40:17 +0300

    Update rule.php

--- rule.php
+++ rule.php
@@ -17,15 +17,15 @@
 /**
  * Implementation of the quizaccess_failgrade plugin.
  *
- * @package   quizaccess_failgrade
- * @copyright 2020 Alexandre Paes Rigão <rigao.com.br>
- * @copyright 2026 quizaccess_failgrade contributors (competency-based mode)
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    quizaccess_failgrade
+ * @copyright  2020 Alexandre Paes Rigão <rigao.com.br>
+ * @copyright  2026 quizaccess_failgrade contributors (competency-based mode)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once ($CFG->libdir . '/gradelib.php');
+require_once($CFG->libdir . '/gradelib.php');
 
 // This work-around is required until Moodle 4.2 is the lowest version we support.
 if (class_exists('\mod_quiz\local\access_rule_base')) {
@@ -33,7 +33,7 @@ if (class_exists('\mod_quiz\local\access_rule_base')) {
     \class_alias('\mod_quiz\local\access_rule_base', 'quiz_access_rule_base');
     \class_alias('\mod_quiz\quiz_settings', 'quiz');
 } else {
-    require_once ($CFG->dirroot . '/mod/quiz/accessrule/accessrulebase.php');
+    require_once($CFG->dirroot . '/mod/quiz/accessrule/accessrulebase.php');
 }
 
 /**
@@ -41,13 +41,12 @@ if (class_exists('\mod_quiz\local\access_rule_base')) {
  * reaching the quiz passing grade (mode 1) or by achieving proficiency in all
  * competencies linked to the quiz course-module (mode 2).
  *
- * @package   quizaccess_failgrade
- * @copyright 2020 Alexandre Paes Rigão <rigao.com.br>
- * @copyright 2026 quizaccess_failgrade contributors (competency-based mode)
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    quizaccess_failgrade
+ * @copyright  2020 Alexandre Paes Rigão <rigao.com.br>
+ * @copyright  2026 quizaccess_failgrade contributors (competency-based mode)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class quizaccess_failgrade extends quiz_access_rule_base
-{
+class quizaccess_failgrade extends quiz_access_rule_base {
     /** @var array Cache for is_finished calculations */
     protected $isfinishedcache = [];
 
@@ -60,11 +59,10 @@ class quizaccess_failgrade extends quiz_access_rule_base
      * @param quiz $quizobj information about the quiz in question.
      * @param int $timenow the time that should be considered as 'now'.
      * @param bool $canignoretimelimits whether the current user is exempt from
-     *      time limits by the mod/quiz:ignoretimelimits capability.
+     * time limits by the mod/quiz:ignoretimelimits capability.
      * @return quiz_access_rule_base|null the rule, if applicable, else null.
      */
-    public static function make(quiz $quizobj, $timenow, $canignoretimelimits)
-    {
+    public static function make(quiz $quizobj, $timenow, $canignoretimelimits) {
         if (empty($quizobj->get_quiz()->failgradeenabled)) {
             return null;
         }
@@ -74,21 +72,20 @@ class quizaccess_failgrade extends quiz_access_rule_base
 
     /**
      * Whether or not a user should be allowed to start a new attempt at this quiz now.
-     * @param int $numattempts the number of previous attempts this user has made.
+     * @param int $numprevattempts the number of previous attempts this user has made.
      * @param object $lastattempt information about the user's last completed attempt.
-     * @return string false if access should be allowed, a message explaining the
-     *      reason if access should be prevented.
+     * @return string|false false if access should be allowed, a message explaining the
+     * reason if access should be prevented.
      */
-    public function prevent_new_attempt($numprevattempts, $lastattempt)
-    {
+    public function prevent_new_attempt($numprevattempts, $lastattempt) {
         if ($this->is_finished($numprevattempts, $lastattempt)) {
             // Trigger an event to log this block.
             $event = \quizaccess_failgrade\event\attempt_blocked_by_failgrade::create([
                 'context' => \context_module::instance($this->quizobj->get_cmid()),
                 'objectid' => $this->quizobj->get_quizid(),
                 'other' => [
-                    'mode' => isset($this->quiz->failgradeenabled) ? $this->quiz->failgradeenabled : 1
-                ]
+                    'mode' => isset($this->quiz->failgradeenabled) ? $this->quiz->failgradeenabled : 1,
+                ],
             ]);
             $event->trigger();
 
@@ -103,10 +100,9 @@ class quizaccess_failgrade extends quiz_access_rule_base
      * There is no obligation to return anything. If it is not appropriate to tell students
      * about this rule, then just return ''.
      * @return mixed a message, or array of messages, explaining the restriction
-     *         (may be '' if no message is appropriate).
+     * (may be '' if no message is appropriate).
      */
-    public function description()
-    {
+    public function description() {
         global $USER;
 
         if ($this->descriptioncache !== null) {
@@ -116,9 +112,9 @@ class quizaccess_failgrade extends quiz_access_rule_base
         $mode = isset($this->quiz->failgradeenabled) ? $this->quiz->failgradeenabled : 1;
 
         if ($mode == 2 && \core_competency\api::is_enabled()) {
-            $cmid     = $this->quizobj->get_cmid();
+            $cmid = $this->quizobj->get_cmid();
             $courseid = $this->quizobj->get_courseid();
-            $userid   = $USER->id;
+            $userid = $USER->id;
 
             $cm_competencies = \core_competency\api::list_course_module_competencies($cmid);
             if (count($cm_competencies) > 0) {
@@ -126,7 +122,9 @@ class quizaccess_failgrade extends quiz_access_rule_base
                 foreach ($cm_competencies as $cm_comp) {
                     $competencyid = $cm_comp->get('competencyid');
                     $usercomp = \core_competency\api::get_user_competency_in_course(
-                        $userid, $competencyid, $courseid
+                        $userid,
+                        $competencyid,
+                        $courseid
                     );
                     if (!$usercomp || !$usercomp->get('proficiency')) {
                         $competency = new \core_competency\competency($competencyid);
@@ -140,7 +138,7 @@ class quizaccess_failgrade extends quiz_access_rule_base
                     $message = get_string('missingcompetencies', 'quizaccess_failgrade', $names_string);
                     return $this->descriptioncache = [
                         '<div class="alert alert-warning" role="alert">' .
-                        '<i class="fa fa-exclamation-triangle"></i> ' . $message . '</div>'
+                        '<i class="fa fa-exclamation-triangle"></i> ' . $message . '</div>',
                     ];
                 }
 
@@ -151,7 +149,7 @@ class quizaccess_failgrade extends quiz_access_rule_base
                     $success_message = get_string('allcompetenciesmet', 'quizaccess_failgrade');
                     return $this->descriptioncache = [
                         '<div class="alert alert-success" role="alert">' .
-                        '<i class="fa fa-check-circle"></i> ' . $success_message . '</div>'
+                        '<i class="fa fa-check-circle"></i> ' . $success_message . '</div>',
                     ];
                 }
 
@@ -169,13 +167,12 @@ class quizaccess_failgrade extends quiz_access_rule_base
      * this quiz, then return true. This is used so we can know whether to display a
      * final grade on the view page. This will only be called if there is not a currently
      * active attempt for this user.
-     * @param int $numattempts the number of previous attempts this user has made.
+     * @param int $numprevattempts the number of previous attempts this user has made.
      * @param object $lastattempt information about the user's last completed attempt.
      * @return bool true if this rule means that this user will never be allowed another
      * attempt at this quiz.
      */
-    public function is_finished($numprevattempts, $lastattempt)
-    {
+    public function is_finished($numprevattempts, $lastattempt) {
         global $USER;
 
         if ($numprevattempts === 0) {
@@ -183,7 +180,7 @@ class quizaccess_failgrade extends quiz_access_rule_base
         }
 
         $userid = isset($lastattempt->userid) ? $lastattempt->userid : $USER->id;
-        
+
         if (isset($this->isfinishedcache[$userid])) {
             return $this->isfinishedcache[$userid];
         }
@@ -195,7 +192,7 @@ class quizaccess_failgrade extends quiz_access_rule_base
             $courseid = $this->quizobj->get_courseid();
             $cm_competencies = \core_competency\api::list_course_module_competencies($cmid);
             $total_competencies = count($cm_competencies);
-            
+
             if ($total_competencies > 0) {
                 $achieved_competencies = 0;
                 foreach ($cm_competencies as $cm_comp) {
@@ -215,114 +212,4 @@ class quizaccess_failgrade extends quiz_access_rule_base
         }
 
         // Grade Mode (mode 1) or fallback
-        $item = grade_item::fetch([
-            'courseid' => $this->quizobj->get_courseid(),
-            'itemtype' => 'mod',
-            'itemmodule' => 'quiz',
-            'iteminstance' => $this->quizobj->get_quizid(),
-            'outcomeid' => null
-        ]);
-
-        if ($item) {
-            $grades = grade_grade::fetch_users_grades($item, [$userid], false);
-            $grade = $grades[$userid];
-
-            if (!empty($grade)) {
-                return $this->isfinishedcache[$userid] = $grade->is_passed($item);
-            }
-        }
-
-        return $this->isfinishedcache[$userid] = false;
-    }
-
-    /**
-     * Add any fields that this rule requires to the quiz settings form. This
-     * method is called from {@link mod_quiz_mod_form::definition()}, while the
-     * security seciton is being built.
-     * @param mod_quiz_mod_form $quizform the quiz settings form that is being built.
-     * @param MoodleQuickForm $mform the wrapped MoodleQuickForm.
-     */
-    public static function add_settings_form_fields(
-        mod_quiz_mod_form $quizform,
-        MoodleQuickForm $mform
-    ) {
-
-        $options = [
-            0 => get_string('failgrademode_disabled', 'quizaccess_failgrade'),
-            1 => get_string('failgrademode_grade', 'quizaccess_failgrade'),
-            2 => get_string('failgrademode_competency', 'quizaccess_failgrade')
-        ];
-
-        $mform->addElement('select', 'failgradeenabled', get_string('failgradeenabled', 'quizaccess_failgrade'), $options);
-        $mform->addHelpButton('failgradeenabled', 'failgradeenabled', 'quizaccess_failgrade');
-    }
-
-    /**
-     * Save any submitted settings when the quiz settings form is submitted. This
-     * is called from {@link quiz_after_add_or_update()} in lib.php.
-     * @param object $quiz the data from the quiz form, including $quiz->id
-     *      which is the id of the quiz being saved.
-     */
-    public static function save_settings($quiz)
-    {
-        global $DB;
-
-        if (empty($quiz->failgradeenabled) || $quiz->failgradeenabled == 0) {
-            $DB->delete_records('quizaccess_failgrade', ['quizid' => $quiz->id]);
-        } else {
-            if (!$DB->record_exists('quizaccess_failgrade', ['quizid' => $quiz->id])) {
-                $record = new stdClass();
-                $record->quizid = $quiz->id;
-                $record->failgradeenabled = $quiz->failgradeenabled;
-                $DB->insert_record('quizaccess_failgrade', $record);
-            } else {
-                $record = $DB->get_record('quizaccess_failgrade', ['quizid' => $quiz->id]);
-                $record->failgradeenabled = $quiz->failgradeenabled;
-                $DB->update_record('quizaccess_failgrade', $record);
-            }
-        }
-    }
-
-    /**
-     * Delete any rule-specific settings when the quiz is deleted. This is called
-     * from {@link quiz_delete_instance()} in lib.php.
-     * @param object $quiz the data from the database, including $quiz->id
-     *      which is the id of the quiz being deleted.
-     * @since Moodle 2.7.1, 2.6.4, 2.5.7
-     */
-    public static function delete_settings($quiz)
-    {
-        global $DB;
-
-        $DB->delete_records('quizaccess_failgrade', ['quizid' => $quiz->id]);
-    }
-
-    /**
-     * Return the bits of SQL needed to load all the settings from all the access
-     * plugins in one DB query. The easiest way to understand what you need to do
-     * here is probalby to read the code of {@link quiz_access_manager::load_settings()}.
-     *
-     * If you have some settings that cannot be loaded in this way, then you can
-     * use the {@link get_extra_settings()} method instead, but that has
-     * performance implications.
-     *
-     * @param int $quizid the id of the quiz we are loading settings for. This
-     *     can also be accessed as quiz.id in the SQL. (quiz is a table alisas for {quiz}.)
-     * @return array with three elements:
-     *     1. fields: any fields to add to the select list. These should be alised
-     *        if neccessary so that the field name starts the name of the plugin.
-     *     2. joins: any joins (should probably be LEFT JOINS) with other tables that
-     *        are needed.
-     *     3. params: array of placeholder values that are needed by the SQL. You must
-     *        used named placeholders, and the placeholder names should start with the
-     *        plugin name, to avoid collisions.
-     */
-    public static function get_settings_sql($quizid)
-    {
-        return [
-            'failgradeenabled',
-            'LEFT JOIN {quizaccess_failgrade} failgrade ON failgrade.quizid = quiz.id',
-            []
-        ];
-    }
-}
+        $item
