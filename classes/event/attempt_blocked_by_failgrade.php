31923708adc69653381f97ae4f698c4c8c57411c
engfeda-ui <eng.feda@gmail.com>
2026-05-17 15:28:09 +0300

    Update attempt_blocked_by_failgrade.php

--- classes/event/attempt_blocked_by_failgrade.php
+++ classes/event/attempt_blocked_by_failgrade.php
@@ -21,26 +21,45 @@ use core\event\base;
 /**
  * Event fired when a student is blocked from attempting a quiz because they already passed.
  *
- * @package   quizaccess_failgrade
- * @copyright 2026 Mahmoud
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    quizaccess_failgrade
+ * @copyright  2026 Mahmoud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class attempt_blocked_by_failgrade extends base {
-
+    /**
+     * Init method.
+     */
     protected function init() {
         $this->data['crud'] = 'r';
         $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
         $this->data['objecttable'] = 'quiz';
     }
 
+    /**
+     * Return localised event name.
+     *
+     * @return string
+     */
     public static function get_name() {
         return get_string('eventattemptblocked', 'quizaccess_failgrade');
     }
 
+    /**
+     * Returns description of what happened.
+     *
+     * @return string
+     */
     public function get_description() {
-        return "The user with id '$this->userid' was blocked from attempting the quiz with course module id '$this->contextinstanceid' because they have already satisfied the passing criteria (mode: {$this->other['mode']}).";
+        return "The user with id '{$this->userid}' was blocked from attempting the quiz " .
+               "with course module id '{$this->contextinstanceid}' because they have " .
+               "already satisfied the passing criteria (mode: {$this->other['mode']}).";
     }
 
+    /**
+     * Return the legacy event log data.
+     *
+     * @return \moodle_url
+     */
     public function get_url() {
         return new \moodle_url('/mod/quiz/view.php', ['id' => $this->contextinstanceid]);
     }
