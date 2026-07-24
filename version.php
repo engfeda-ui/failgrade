9638b1f2d54cda114b4dcc6df311c7dfe9b6339b
fody-dev <mahmoud.feda@gmail.com>
2026-07-24 23:53:47 +0300

    chore(release): bump version to 2.3.0 (2026072401) for quizaccess_failgrade_ext

--- version.php
+++ version.php
@@ -27,13 +27,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026070500;
+$plugin->version   = 2026072401;
 $plugin->requires  = 2022041900; // Moodle 4.0 minimum.
 $plugin->supported = [400, 500];  // Moodle 4.0 – 5.0.
 $plugin->component = 'quizaccess_failgrade_ext';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v2.2.1';
+$plugin->release   = 'v2.3.0';
 
 $plugin->dependencies = [
-    'qbank_comp_ext' => 2026052500,
+    'qbank_comp_ext' => 2026070500,
 ];
